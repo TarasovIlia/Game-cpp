00c6a8601a7ef4bf1f0e55c257375313aff5a507
TarasovIlia <tarasov002@gmail.com>
2021-09-16 22:27:55 +0300

    split function

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ using namespace std;
         {"HEALTH", 100},
         {"POWER", 8},
         {"EXP", 0},
+        {"WARRIORS_KILLED", 0},
     };
     map<string, int>INVENTORY = {
         {"HELMET", 1},
@@ -30,9 +31,60 @@ using namespace std;
         {"HEALTH", 60},
         {"POWER", 3},
     };
-void hit();
-int main() 
-{
+
+    int HEALING_POTION_POINT = 14;
+
+void hit() {
+    ENEMY_WARRIOR_STAT["HEALTH"] -= HERO_STAT["POWER"];
+    HERO_STAT["HEALTH"] -= ENEMY_WARRIOR_STAT["POWER"];
+}
+
+void showStatus() {
+    map<string, int>::iterator iter = HERO_STAT.begin();
+    cout << "Yuore status: " <<  endl;
+    for(int i = 0; iter != HERO_STAT.end(); iter++ , i++)
+    {
+        cout << i+1 << ". " << iter->first << ": " << iter->second << endl;
+    }
+}
+
+void showInventory() {
+    map<string, int>::iterator iter = INVENTORY.begin();
+    cout << "Yuore inventory: " <<  endl;
+    for(int i = 0; iter != INVENTORY.end(); iter++ , i++)
+    {
+        cout << i+1 << ". " << iter->first << ": " << iter->second << endl;
+    }
+}
+
+void useHealingPotions() {
+    if (INVENTORY["HEALING_POTION"] > 0) {
+        HERO_STAT["HEALTH"] += HEALING_POTION_POINT;
+        INVENTORY["HEALING_POTION"] -= 1;
+    }
+    else {
+        cout << "You no longer have the potion" << endl;
+    }
+}
+
+void inventoryAction() {
+    int inventory_action;
+    cin >> inventory_action;
+    if (inventory_action == 3){
+        useHealingPotions();
+    }
+}
+
+void showSpell() {
+    map<string, int>::iterator iter = HERO_SPELL.begin();
+    cout << "Yuore spell: " <<  endl;
+    for(int i = 0; iter != HERO_SPELL.end(); iter++ , i++)
+    {
+        cout << i+1 << ". " << iter->first << ": " << iter->second << endl;
+    }
+}
+
+int main() {
     int basicExpBonus = 8;
     double caf = 1.2;
     string action;
@@ -47,13 +99,14 @@ int main()
 
     while(HERO_STAT["HEALTH"] > 0 && action != "end")
     {
-        if(ENEMY_WARRIOR_STAT["HEALTS"] <= 0)
+        if(ENEMY_WARRIOR_STAT["HEALTH"] <= 0)
         {
             cout << "\n\nYoure win!" << endl;
-            ENEMY_WARRIOR_STAT["HEALTS"] = ENEMY_WARRIOR_STAT["BASIC_HEALTS"] * caf;
+            ENEMY_WARRIOR_STAT["HEALTH"] = ENEMY_WARRIOR_STAT["BASIC_HEALTH"] * caf;
             ENEMY_WARRIOR_STAT["POWER"] += caf;
             HERO_STAT["EXP"] += 12*caf;
-            caf += 0.3;
+            caf += 0.2;
+            HERO_STAT["WARRIORS_KILLED"] += 1;
 
             srand(static_cast<unsigned int>(time(0)));
             random_shuffle(cointChance.begin(), cointChance.end());
@@ -68,48 +121,28 @@ int main()
             cout << "But there is new enemy." << endl;
             
         }
-        cout << "\n\nEnter action: \n" << endl;
-        cout << ACTION[1] << endl 
+        cout << "\n\nEnemy healts: " << ENEMY_WARRIOR_STAT["HEALTH"] << endl;
+        cout << "\nEnter action: \n" << endl;
         cout << "1. Hit" << endl;
-        cout << "2. Healing Spell" << endl;
+        cout << "2. Spell" << endl;
         cout << "3. Status" << endl;
         cout << "4. Inventory\n\n" << endl;
-        cout << "Enemy healts: " << ENEMY_WARRIOR_STAT["HEALTS"] << endl;
         cin >> action;
-        if(action == "1")
-        {
-            ENEMY_WARRIOR_STAT["HEALTS"] -= HERO_STAT["POWER"];
-            HERO_STAT["HEALTH"] -= ENEMY_WARRIOR_STAT["POWER"];
+        if(action == "1") {
+            hit();
         }
+
         else if(action == "2")
         {
-            if (INVENTORY["HEALING_POTION"] > 0)
-            {
-                HERO_STAT["HEALTH"] += HERO_STAT["HEALING_SPELL"];
-                INVENTORY["HEALING_POTION"] -= 1;
-            }
-            else
-            {
-                cout << "You no longer have the potion" << endl;
-            }
+            showSpell();
         }
-        else if(action == "3")
-        {
-            map<string, int>::iterator iter = HERO_STAT.begin();
-            cout << "Yuore status: " <<  endl;
-            for(int i = 0; iter != HERO_STAT.end(); iter++ , i++)
-            {
-                cout << i+1 << ". " << iter->first << ": " << iter->second << endl;
-            }
+        else if(action == "3") {
+            showStatus();
         }
-        else if(action == "4")
-        {
-            map<string, int>::iterator iter = INVENTORY.begin();
-            cout << "Yuore inventory: " <<  endl;
-            for(int i = 0; iter != INVENTORY.end(); iter++ , i++)
-            {
-                cout << i+1 << ". " << iter->first << ": " << iter->second << endl;
-            }
+        else if(action == "4") {
+            showInventory();
+            cout << "Enter item number to use it: " << endl;
+            inventoryAction();
         }
     }
     if (HERO_STAT["HEALTH"] <= 0)
@@ -118,3 +151,4 @@ int main()
     }
     return 0;
 }
+
